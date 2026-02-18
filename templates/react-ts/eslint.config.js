ca280d341caaa2ca44b1aa22c3d4bfea992b7b62
Numer <numerpriv@gmail.com>
2026-02-18 01:08:04 +0100

    chore(templates/react-ts): `@antfu/eslint-config`

--- templates/react-ts/eslint.config.js
+++ templates/react-ts/eslint.config.js
@@ -1,23 +1,26 @@
-import js from "@eslint/js";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import antfu from '@antfu/eslint-config';
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+export default antfu({
+  react: true,
+  stylistic: {
+    semi: true,
+    quotes: 'single',
+    indent: 2,
   },
-]);
+  rules: {
+    'style/quotes': ['error', 'single'],
+    'test/prefer-lowercase-title': 'off',
+    'style/jsx-one-expression-per-line': ['off', { allow: 'single-line' }],
+    'style/jsx-curly-brace-presence': ['error', { props: 'always' }],
+    'style/jsx-curly-spacing': [
+      'warn',
+      {
+        when: 'never',
+        children: {
+          when: 'always',
+        },
+      },
+    ],
+    'antfu/top-level-function': 'off',
+  },
+});
