bcf24e67e7f823177c63c25f61a944b350ea4f25
Numer <numerpriv@gmail.com>
2026-04-24 11:36:40 +0200

    test: add example test cases

---
+++ templates/react-ts/src/test/example_node.test.tsx
@@ -0,0 +1,9 @@
+// Its only an example and we don't need to use this docblock because
+// by default environment is set to node
+// @vitest-environment node
+describe('Example', () => {
+  it('Basic test', () => {
+    const a = 5;
+    expect(a).toBe(5);
+  });
+});
